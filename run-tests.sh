65a1126bd98aa813aea7e9b3399aeeb5d6388bd9
lulalachen <lullalachen@gmail.com>
2016-06-06 01:39:27 +0800

    chore(travis): add npm install in run-test.sh

--- run-tests.sh
+++ run-tests.sh
@@ -3,13 +3,13 @@
 echo "\033[32m > ======================================================="
 echo "\033[32m > [es6-node-server] [Processing] 🚀"
 echo "\033[32m > ======================================================= \033[0m"
-
 cd es6-node-server
+npm install
 npm run test:coverage
 
 echo "\033[32m > ======================================================="
 echo "\033[32m > [npm-packages] [Processing] 🚀"
 echo "\033[32m > ======================================================= \033[0m"
-
 cd ../npm-packages
+npm install
 npm run test:coverage
